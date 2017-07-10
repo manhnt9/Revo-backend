625f948a07c8acb492c948ab5c48e37fbb49e589
ManhNT <zer0.x@outlook.com>
2017-07-10 17:23:50 +0700

    Check session before processing

--- app/controllers/api/form_controller.rb
+++ app/controllers/api/form_controller.rb
@@ -1,16 +1,20 @@
 class Api::FormController < ApplicationController
   def index
-    forms = Form.all
-    render :json => forms.to_json()
+    if Session.find_by_token(params[:token])
+      forms = Form.all
+      render :json => forms.to_json()
+    end
   end
 
   def show
-    form = Form.find_by_id(params[:id])
-    render :json => form.to_json()
+    if Session.find_by_token(params[:token])
+      form = Form.find_by_id(params[:id])
+      render :json => form.to_json()
+    end
   end
 
   def create
     Form.create!({ name: params[:name], parent_name: params[:parent_name], dob: params[:dob], phone: params[:phone], 
-    social: params[:social], course: params[:course], note: params[:note], email: params[:email]});
+    social: params[:social], course: params[:course], note: params[:note], email: params[:email] });
   end
 end
