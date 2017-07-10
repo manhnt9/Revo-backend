75e844794dad2a919e3d4efcbb0edc5df013c578
ManhNT <zer0.x@outlook.com>
2017-07-10 11:13:20 +0700

    Implement session destroy

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -5,12 +5,16 @@ class Api::SessionController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session = Session.create!({ user_id: user.id });
+      session = Session.create!({ user_id: user.id })
       reply = {'token': session.token}
       render :json => reply
     else
       reply = {'result': 'failure'}
       render :json => reply
     end
+  end
+
+  def destroy
+    Session.find_by_token(params[:token]).destroy()
   end
 end
