8e2f459806390b4e452865aa15fe6acb8eefe732
ManhNT <zer0.x@outlook.com>
2017-07-10 11:56:53 +0700

    Implement individual form fetching

--- app/controllers/api/form_controller.rb
+++ app/controllers/api/form_controller.rb
@@ -4,6 +4,11 @@ class Api::FormController < ApplicationController
     render :json => forms.to_json()
   end
 
+  def show
+    form = Form.find_by_id(params[:id])
+    render :json => form.to_json()
+  end
+
   def create
     Form.create!({ name: params[:name], parent_name: params[:parent_name], dob: params[:dob], phone: params[:phone], 
     social: params[:social], course: params[:course], note: params[:note], email: params[:email]});
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :students
   namespace :api, defaults: { format: :json } do
     get '/form/', to: 'form#index'
+    get '/form/:id', to: 'form#show'
     post '/form/', to: 'form#create'
     post '/session/', to: 'session#create'
     delete '/session/', to: 'session#destroy'
